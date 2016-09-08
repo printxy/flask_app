935f4e1ff29aaf47fb02f29957648107f6e5a5c5
printxy <iemail.wy@gmail.com>
2016-09-08 20:03:14 +0800

    增加分页显示文章

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <iemail_wy@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
